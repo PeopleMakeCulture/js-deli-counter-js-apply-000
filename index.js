591431a0e7b19736ddce086becc02db6fecc9a7b
Jing Cao <jingcao.yale@gmail.com>
2018-06-28 15:44:57 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,15 @@
+function takeANumber(line, person){
+  //adds a person to line
+  line.push(person);
+  console.log(person);
+  //returns message
+  console.log ("Welcome, ${person}.") //You are number ${line.length + 1} in line."
+}
+
+function nowServing(line){
+  if (line.length = 0){
+    return 'There is nobody waiting to be served!'
+  }
+}
+
+function currentLine(){}
